a2539b6d6b69dd8583226a6f2ccfee3f07f0ed85
trio010416 <95908666+trio010416@users.noreply.github.com>
2022-01-06 14:22:41 +0700

    Merge remote-tracking branch 'origin/master'

--- data_input/data_input.py
+++ data_input/data_input.py
@@ -71,11 +71,63 @@ def wtf_template2():
                            form=form)
 
 
-@data_input.route('/wtf_template3', methods=['GET', 'POST'])
+@data_input.route('/wtf_start', methods=['GET', 'POST'])
+def wtf_start():
+    # otd = request.args.get('12')
+    # doc = request.args.get('23')
+
+    result_podr2 = db.select(sql.sql_podr)
+    result_rasp = db.select(sql.sql_it_rasp.format(doc=0))
+    visible_ = ''
+    if result_rasp == []:
+        result_rasp.append('')
+        result_rasp.append('')
+        result_rasp.append('')
+        result_rasp.append('0')
+        result_rasp.append('0')
+        result_rasp.append('')
+        result_rasp = (result_rasp, )
+    else:
+        visible_ = 'style=display:none;'
+    # result_fio = db.select(sql.sql_fio.format(otd=0))
+    # if result_fio == []:
+    result_fio=([])
+    result_fio.append(0)
+    result_fio.append('')
+    result_fio = (result_fio, )
+    result_podr = db.select(sql.sql_podr_selected.format(otd=0))
+    form = WtfTemplate3()
+    return render_template('wtf_start.html',
+                           form=form,
+                           result_rasp=result_rasp,
+                           result_podr2=result_podr2,
+                           result_fio=result_fio,
+                           result_podr=result_podr,
+                           visible_=visible_)
+
+
+@data_input.route('/wtf_template3/', methods=['GET', 'POST'])
 def wtf_template3():
     otd = request.args.get('otd')
-    doc = request.args.get('doc')
+    result_notd = db.select(sql.sql_podr_selected.format(otd=otd))
+    notd = result_notd[0]
+    print(result_notd)
+    result_doc = db.select(sql.sql_doc.format(otd=otd))
+    doc = request.args.get('doc') or utils.list_to_int(result_doc)
+
     lpu = utils.list_to_int(db.select(sql.sql_lpu_selected.format(otd=otd)))
+    result_rasp = db.select(sql.sql_it_rasp.format(doc=doc))
+    visible_ = ''
+    if result_rasp == []:
+        result_rasp.append('')
+        result_rasp.append('')
+        result_rasp.append('')
+        result_rasp.append('0')
+        result_rasp.append('0')
+        result_rasp.append('')
+        result_rasp = (result_rasp, )
+    else:
+        visible_ = 'style=display:none;'
 
     if request.method == 'POST':
         if request.form['btn'] == 'DelRspBlc':
@@ -129,42 +181,34 @@ def wtf_template3():
 
         if request.form['btn'] == 'UpdRegimeWork':
             spz = request.form.get('UpdSpz')
-            if spz == None:
+            if spz == "":
                 spz = 0
             room = request.form.get('UpdRoom')
-            if room == None:
+            if room == "":
                 room = 0
             interval1 = request.form.get('UpdNoEvenDay')
-            if interval1 == None:
+            if interval1 == "":
                 interval1 = 0
-                print(interval1)
             interval2 = request.form.get('UpdEvenDay')
-            if interval2 == None:
+            if interval2 == "":
                 interval2 = 0
-
-            ntv = request.form.get('NTV')
-            nlist = request.form.get('NLIST')
-
+            ntv = request.form.get('UpdNtv')
+            nlist = request.form.get('UpdNlist')
+            visible_ = 'style=display:none;'
             db.write(sql_del_it_rasp.format(doc=doc))
-            print(nlist)
             print(sql_ins_it_rasp.format(doc=doc, lpu=lpu, otd=otd, spz=spz, room=room, interval1=interval1, interval2=interval2, ntv=ntv, nlist=nlist))
-            # db.write(sql_ins_it_rasp.format(doc=doc, lpu=lpu, otd=otd, spz=spz, room=room, interval1=interval1, interval2=interval2, ntv=ntv, nlist=nlist))
-            # return redirect(url_for("data_input.wtf_template3", otd=otd, doc=doc))
+            db.write(sql_ins_it_rasp.format(doc=doc, lpu=lpu, otd=otd, spz=spz, room=room, interval1=interval1, interval2=interval2, ntv=ntv, nlist=nlist))
+            return redirect(url_for("data_input.wtf_template3", otd=otd, doc=doc, visible_=visible_))
+
+        if request.form['btn'] == 'DelRegimeWork':
+            visible_ = ''
+            db.write(sql_del_it_rasp.format(doc=doc))
+            return redirect(url_for("data_input.wtf_template3", otd=otd, doc=doc, visible_=visible_))
 
     result_rsn = db.select(sql.sql_rsp_rsn)
     result_rsp_blc = db.select(sql.sql_rsp_blc.format(doc=doc))
     result_room = db.select(sql.sql_room.format(doc=doc))
     result_spz = db.select(sql.sql_spz)
-
-    result_rasp = db.select(sql.sql_it_rasp.format(doc=doc))
-    if result_rasp == []:
-        result_rasp.append('')
-        result_rasp.append('')
-        result_rasp.append('')
-        result_rasp.append('0')
-        result_rasp.append('0')
-        result_rasp.append('')
-        result_rasp = (result_rasp, )
 
     result_duty = db.select(sql.sql_it_rasp_duty.format(doc=doc))
     result_time = db.select(sql.sql_interval_time)
@@ -177,7 +221,7 @@ def wtf_template3():
     result_podr2 = db.select(sql.sql_podr)
 
     form = WtfTemplate3()
-
+    print(visible_)
     return render_template('wtf_template3.html',
                            result_fio=result_fio,
                            result_rsn=result_rsn,
@@ -189,8 +233,10 @@ def wtf_template3():
                            result_spz=result_spz,
                            fioSotrudnika=fioSotrudnika,
                            doc=doc,
+                           otd=otd,
                            lpu=lpu,
                            form=form,
+                           visible_=visible_,
                            result_podr=result_podr,
                            result_podr2=result_podr2)
 
