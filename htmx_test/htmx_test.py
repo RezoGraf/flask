8a6b352e5bcb8edda4337dda72cf9ae072972d11
trio010416 <95908666+trio010416@users.noreply.github.com>
2022-01-25 13:13:06 +0700

    fggfg

--- htmx_test/htmx_test.py
+++ htmx_test/htmx_test.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, Blueprint
 import json
 import os.path
 import db, sql
+from data_input.sql_data_input import sql_upd_it_rasp_grf
+from dateutil import parser
+from datetime import date
+import calendar
 
 data = ["Один", "Тор"]
 
@@ -48,14 +52,48 @@ def index():
 @htmx_test.route("/table_view")
 def table_view():
     """отображение таблицы с расписанием, ничего интересного
-
     Returns:
         html страницу (htmx_tableview.html) с таблицей, смотреть внимательно
     """    
-    table_view_all = db.select(sql.sql_htmx_text_tablevew)
-    otd=12
-    return render_template("htmx_tableview.html", table_view_all=table_view_all)
+    current_date = date.today()
+    current_year = parser.parse(current_date.strftime('%m/%d/%y')).strftime("%Y")
+    current_month = parser.parse(current_date.strftime('%m/%d/%y')).strftime("%m")
+    all_day = calendar.monthrange(int(current_year), int(current_month))[1]
+    # result_otd = db.select(sql.sql_allOtd) 
+    russianDayWeek = {'Mon':'Пн.' , 'Tue':'Вт.' , 'Wed':'Ср.' , 'Thu':'Чт.' , 'Fri':'Пт.' , 'Sat':'Сб.' , 'Sun':'Вс.'}
+    
+    result_th = {}
+    result_style_td = {}
+    for i in range(all_day):
+            i+=1
+            dt = f'{str(current_month)}.{str(i)}.{str(current_year)}'
+            ans = parser.parse(dt).strftime("%a")
+            pa = russianDayWeek[ans]
+            if i<10 :
+                p=f'0{i}'
+            else:
+                p=str(i)  
+            value_ = f"""{p}  {pa}""" 
+            key_ = f'day{str(i)}'
+            if pa=='Вс.' or pa=='Сб.' :
+                class_th='table-success'
+            else:
+                class_th='table-light'
+            result_th[key_] = [class_th,value_]  
+    otd=12           
+    table_view_all = db.select(sql.sql_TabelWorkTime.format(otd=otd, EYear=current_year, EMonth=current_month))
+    print(table_view_all)  
+    
 
+    # i = 1
+    # result_td = {}
+    # while i < (len(table_view_all)):
+    #     key_ = table_view_all
+    #     result_th[key_] = [class_th,value_]      
+      
+    return render_template("htmx_tableview.html", 
+                           table_view_all = table_view_all,
+                           result_th = result_th)
 
 
 @htmx_test.route("/table_view/edit", methods=["GET", "POST"])
@@ -75,16 +113,24 @@ def table_edit():
     """    
     if request.method == 'POST':
         id_td = request.args.get('id_td')
+        s_id_td = id_td[2:4]
+        if int(s_id_td)<10 :
+            s_id_td = s_id_td[1:2]
+        s_id_td = f'day{s_id_td}'    
         id_grf = request.args.get('id_grf')
         rasp_id = request.form.get('rasp_id')
+        rasp_id_visible =db.select( sql.sql_interval_time_current.format(id=rasp_id))
+        rasp_id_visible = rasp_id_visible[0][0]
+        db.write(sql_upd_it_rasp_grf.format(day_col=s_id_td, day_zn=rasp_id, id_grf=id_grf))
+        
         response = f"""
             <div name="id_grf" 
                 hx-target="#{id_td}" 
-                hx-swap="innerHTML" hx-get="table_view/edit?id_td={id_td}&id_grf={id_grf}">{rasp_id}
+                hx-swap="innerHTML" hx-get="table_view/edit?id_td={id_td}&id_grf={id_grf}">{rasp_id_visible}
             </div>
         """
         return response
-    else:
+    else:                
         id_td = request.args.get('id_td')
         id_grf = request.args.get('id_grf')
         list_of_time = db.select(sql.sql_interval_time)
@@ -94,6 +140,7 @@ def table_edit():
             option = f"""<option value="{list_of_time[i][0]}">{list_of_time[i][1]}</option>"""
             list_of_options = list_of_options + option
             i += 1
+        print(list_of_options)   
         response = f"""
                 <div>
                     <select name="rasp_id" hx-post="table_view/edit?id_grf={id_grf}&id_td={id_td}" 
