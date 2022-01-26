a17810d6591187e8f3b701d6286b70cfcc710de4
RezoGraf <ctsaverno@gmail.com>
2022-01-26 09:53:08 +0700

    function

--- db.py
+++ db.py
@@ -58,3 +58,25 @@ def select_dicts_in_turple(sql):
     cur.close()
     del cur
     return result2
+
+
+def select_dicts_in_turple2(sql):
+    con = fdb.connect(dsn=config.dsn,
+                      user=config.user,
+                      password=config.password,
+                      charset=config.charset)
+    cur = con.cursor()
+    cur.execute(sql)
+    result = {}
+    fieldIndices = range(len(cur.description))
+    selectFields = ()
+    for fieldDesc in cur.description:
+        selectFields = *selectFields, fieldDesc[fdb.DESCRIPTION_NAME]
+    result2 = ()
+    for row in cur:
+        for fieldIndex in fieldIndices:
+            result[selectFields[fieldIndex]] = row[fieldIndex]
+        result2 = (*result2, result)
+    cur.close()
+    del cur
+    return result2
