6b1e33e0a3783b37edf27b6726d8b404be53f262
RezoGraf <ctsaverno@gmail.com>
2022-02-18 13:19:36 +0700

    in_work fix

--- app/in_work/in_work.py
+++ app/in_work/in_work.py
@@ -20,8 +20,24 @@ def main():
     menu = session['menu']
     today = datetime.today().strftime('%d.%m.%Y')
     all_today = db.select_dicts_in_turple_with_description(sql_in_work.sql_all_today.format(today = today))
-    print(all_today[0])
-    return render_template('in_work.html', menu=menu, table = all_today)
+    
+    all_otd = db.select(sql_in_work.sql_all_otd)
+    select_otd = """<option selected></option>"""
+    for x in all_otd:
+        select_string = f"""<option style="font-size:15px">{x[0]}</option>"""
+        select_otd += select_string
+    all_select_otd = f"""<select style="font-size:15px" class="form-select" name="reason_filter" id="myInputReason"
+    name="select_otd">{select_otd}</select>"""
+
+    all_podr = db.select(sql_in_work.sql_all_podr)
+    select_podr = """<option selected style="font-size:15px"></option>"""
+    for x in all_podr:
+        select_string = f"""<option style="font-size:15px">{x[0]}</option>"""
+        select_podr += select_string
+    all_select_podr = f"""<select style="font-size:15px" class="form-select" name="podr_filter" id="myInputPodr"
+    name="select_otd">{select_podr}</select>"""
+
+    return render_template('in_work.html', menu=menu, table = all_today, all_select_otd=all_select_otd, all_select_podr=all_select_podr)
 
 
 
--- app/in_work/sql_in_work.py
+++ app/in_work/sql_in_work.py
@@ -14,6 +14,18 @@ Where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc)
 order by SNLPU,NOTD,NMPP"""
 
 
+sql_all_otd = """Select distinct np_otd.notd
+From it_rasp,np_otd,n_doc,n_mpp 
+Where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc) 
+  and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1) and np_otd.notd is not Null"""
+
+sql_all_podr = """Select distinct
+ (select SNLPU from n_lpu where ter=5 and N_LPU.LPU=N_DOC.LPU) as SNLPU
+From it_rasp,np_otd,n_doc,n_mpp 
+Where (it_rasp.otd=np_otd.otd) and (it_rasp.doc=n_doc.doc) 
+  and (n_doc.mpp=n_mpp.mpp) and (n_doc.pv=1)"""
+
+
 # sql_all_today = """Select it_rasp.doc,np_otd.notd,n_mpp.nmpp,n_mpp.tel, 
 #  (select ndlj as nspz from n_dlj where n_doc.dolj=n_dlj.dlj),
 #  (select nroom_kr from room where room.id=it_rasp.room),
